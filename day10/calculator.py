8616a7fee167f56e180764dc8b4a659f1b1dd148
Ethan <zycc2727@gmail.com>
2022-10-01 00:16:51 +1000

    method 1

--- day10/calculator.py
+++ day10/calculator.py
@@ -1,2 +1,34 @@
+
+
 def addition(a, b):
     return a + b
+
+
+def substruct(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def devide(a, b):
+    return a / b
+
+
+operations = {
+    "+": addition,
+    "-": substruct,
+    "*": multiply,
+    "/": devide
+}
+
+num_1 = int(input("what is the first nunber: "))
+for i in operations:
+    print(i)
+symbol = input("what is your opeartor? ")
+num_2 = int(input("what is the second nunber: "))
+
+function = operations[symbol]
+answer = function(num_1, num_2)
+print(answer)
