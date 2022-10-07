b4fad1252eb0b1ed5bd7a9e56d6c10c18df23cc7
Ethan <zycc2727@gmail.com>
2022-10-08 10:45:08 +1100

    random walk with random RGB color

--- turtle project/main.py
+++ turtle project/main.py
@@ -1,14 +1,24 @@
+import turtle
 from turtle import Turtle, Screen
-from random import choice
+from random import choice, randint
 timmy = Turtle()
 timmy.shape("circle")
 timmy.pensize(5)
-color = ['red', 'green', 'blue', 'yellow', 'orange', 'purple']
 angle = [90, 270]
+turtle.colormode(255)
+
+
+def random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
 
 for i in range(100):
     timmy.forward(10)
-    timmy.color(choice(color))
+    timmy.color((random_color()))
     timmy.right(choice(angle))
     timmy.forward(10)
     timmy.speed(100)
