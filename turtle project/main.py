8324157ba2c54a071a5b506b7904462579394c9a
Ethan <zycc2727@gmail.com>
2022-10-08 10:33:26 +1100

    random walk

--- turtle project/main.py
+++ turtle project/main.py
@@ -2,20 +2,16 @@ from turtle import Turtle, Screen
 from random import choice
 timmy = Turtle()
 timmy.shape("circle")
-
+timmy.pensize(5)
 color = ['red', 'green', 'blue', 'yellow', 'orange', 'purple']
+angle = [90, 270]
 
-
-def draw(x):
-    angle = 360 / x
-    for i in range(x):
-        timmy.forward(50)
-        timmy.right(angle)
-
-
-for i in range(3, 10):
-    draw(i)
+for i in range(100):
+    timmy.forward(10)
     timmy.color(choice(color))
+    timmy.right(choice(angle))
+    timmy.forward(10)
+    timmy.speed(100)
 
 
 screen = Screen()
