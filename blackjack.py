9eb67e470503dbfea5a003ebde2d23932be5d0b7
Ethan <zycc2727@gmail.com>
2022-10-09 22:30:58 +1100

    code complete

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,5 @@
 from random import choice
 
-
 cards_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
@@ -17,13 +16,50 @@ def calculate_score(cards):
     return sum(cards)
 
 
+def compare(userscore, dealerscore):
+    if userscore == dealerscore:
+        return "draw"
+    elif dealerscore == 0:
+        return "you lose, dealer blackjack"
+    elif userscore == 0:
+        return "you win. blackjack"
+    elif userscore > 21:
+        return "you lose, you went over"
+    elif dealerscore > 21:
+        return "you win, dealer went over"
+    elif dealerscore > userscore:
+        return "dealer win"
+    else:
+        return "you win"
+
+
 user_cards = list()
 dealer_cards = list()
+
 for i in range(0, 2):
     user_cards.append(deal_card())
     dealer_cards.append(deal_card())
 
-print(calculate_score(user_cards))
-print(calculate_score(dealer_cards))
-print(f'your cards: {user_cards}')
-print(f'deal cards: {dealer_cards}')
+game_on = True
+while game_on:
+    user_score = calculate_score(user_cards)
+    dealer_score = calculate_score(dealer_cards)
+    print(f'your cards: {user_cards}, score is {user_score}')
+    print(f'deal cards: {dealer_cards}, score is {dealer_score}')
+
+    if user_score == 0 or dealer_score == 0 or user_score > 21:
+        game_on = False
+    else:
+        one_more = input("one more card?")
+        if one_more == 'y':
+            user_cards.append(deal_card())
+            calculate_score(user_cards)
+        if one_more == 'n':
+            game_on = False
+
+while dealer_score != 0 and dealer_score < 17:
+    dealer_cards.append(deal_card())
+    dealer_score = calculate_score(dealer_cards)
+
+print(dealer_score)
+print(compare(user_score, dealer_score))
