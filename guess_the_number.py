41c6f08695ec66157e96dd5afdd0564a9ffd3341
Ethan <zycc2727@gmail.com>
2022-10-12 11:32:22 +1100

    fail

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,16 +1,43 @@
 from random import choice
+HARD_LEVEL = 5
+EASY_LEVEL = 10
 
 
 def difficulty():
     level = input("difficulty: ")
-    if level == 'easy':
-        return 10
-    if level == 'hard':
-        return 5
+    if level == '1':
+        return HARD_LEVEL
+    if level == '2':
+        return EASY_LEVEL
+
+
+def too_high():
+    print("too high")
+    print(f'you have {attempts -1 - i} attempts remaining')
+
+
+def too_low():
+    print("too low")
+    print(f'you have {attempts -1 - i} attempts remaining')
+
+
+def correct_guess():
+    print("you made it")
+    pass
 
 
 attempts = difficulty()
 number_to_guess = choice(range(0, 101))
 print(f"you have {attempts} attempts to guess the number")
 print(number_to_guess)
+print(type(attempts))
 
+for i in range(1, attempts + 1):
+    guessed_number = int(input("guess:"))
+    if guessed_number > number_to_guess:
+        too_high()
+    elif guessed_number < number_to_guess:
+        too_low()
+    else:
+        correct_guess()
+        break
