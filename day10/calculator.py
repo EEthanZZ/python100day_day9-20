86f862c72418019300f89bdbc22373255e2f8f43
ethan <yczhu@yahoo.com>
2022-10-13 22:12:23 +1100

    git email correction

--- day10/calculator.py
+++ day10/calculator.py
@@ -23,12 +23,26 @@ operations = {
     "/": devide
 }
 
-num_1 = int(input("what is the first nunber: "))
-for i in operations:
-    print(i)
-symbol = input("what is your opeartor? ")
-num_2 = int(input("what is the second nunber: "))
 
-function = operations[symbol]
-answer = function(num_1, num_2)
-print(answer)
+def calculation():
+    num_1 = int(input("what is the first nunber: "))
+    for i in operations:
+        print(i)
+    should_continue = True
+    while should_continue:
+        symbol = input("what is your opeartor? ")
+        num_2 = int(input("what is the second nunber: "))
+
+        function = operations[symbol]
+        answer = function(num_1, num_2)
+        print(f'{num_1}{symbol}{num_2} = {answer}')
+        if input("continue yes or no") == 'y':
+            num_1 = answer
+        else:
+            should_continue = False
+            calculation()
+
+
+if input("start the calculator? ") == "y":
+    calculation()
+
