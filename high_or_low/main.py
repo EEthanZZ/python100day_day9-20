4ea99c6079c960c3daa7733451baa0f46714d325
EEthanZZ <yczhu@yahoo.com>
2022-10-13 23:18:05 +1100

    check_answer function

--- high_or_low/main.py
+++ high_or_low/main.py
@@ -1,7 +1,23 @@
 from game_data import data
 from random import randint, choice
+from art import *
+
+
+def check_answer(a, b):
+    if a > b:
+        return a
+    else:
+        return b
 # construct two compares
 compare_a = choice(data)
 compare_b = choice(data)
 print(f"Compare A: {compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}")
+print(vs)
 print(f"Compare B: {compare_b['name']}, a {compare_b['description']}, from {compare_b['country']}")
+a_followers = compare_a['follower_count']
+b_followers = compare_b['follower_count']
+x = input("who has more followers? ")
+check_answer(a_followers, b_followers)
+# check the answer:
+# if a > b, return a / if b > a, return b
+
