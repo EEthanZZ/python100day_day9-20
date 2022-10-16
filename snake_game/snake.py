59cc64ba28cb4f535a22adef803c67c05bd3e6b1
EEthanZZ <yczhu@yahoo.com>
2022-10-16 21:34:27 +1100

    snake initial movemennt function

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -13,3 +13,10 @@ class Snake:
             snake_body.penup()
             snake_body.goto(40 - i * 20, 0)
             self.body_blocks.append(snake_body)
+
+    def movement(self):
+        for i in range(len(self.body_blocks) - 1, 0, -1):
+            new_x = self.body_blocks[i - 1].xcor()
+            new_y = self.body_blocks[i - 1].ycor()
+            self.body_blocks[i].goto(new_x, new_y)
+        self.body_blocks[0].forward(20)
