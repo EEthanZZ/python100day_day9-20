2f70b7082222bc7af18fa302bd876402a4635a8e
EEthanZZ <yczhu@yahoo.com>
2022-10-16 21:32:21 +1100

    remove the code to the snake class and create the constructor

--- snake_game/snake_game.py
+++ snake_game/snake_game.py
@@ -17,8 +17,9 @@ task breakdown
 """
 import time
 from turtle import Turtle, Screen
+from snake import Snake
 # move the snake#
-body_blocks = []
+
 
 # for loop to create the snake body#
 screen = Screen()
@@ -28,24 +29,14 @@ screen.title("Welcome to the snake game")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-for i in range(3):
-    snake_body = Turtle('square')
-    snake_body.color('white')
-    snake_body.penup()
-    snake_body.goto(40 - i * 20, 0)
-    body_blocks.append(snake_body)
+snake = Snake()
 
 
 is_game_on = True
 while is_game_on:
     screen.update()
     time.sleep(0.1)
-    for i in range(len(body_blocks) - 1, 0, -1):
-        new_x = body_blocks[i-1].xcor()
-        new_y = body_blocks[i-1].ycor()
-        body_blocks[i].goto(new_x, new_y)
-    body_blocks[0].forward(20)
-    body_blocks[0].left(90)
+    snake.movement()
 
 
 screen.exitonclick()
