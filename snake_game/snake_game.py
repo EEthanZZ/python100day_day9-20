4ec99ae0c81b8f512f04755c755caaf26c070949
EEthanZZ <yczhu@yahoo.com>
2022-10-16 17:55:42 +1100

    group the blocks into one piece and modify the screen refresh frequency

--- snake_game/snake_game.py
+++ snake_game/snake_game.py
@@ -15,19 +15,34 @@ task breakdown
 
 7.detect collision with tail
 """
+import time
 from turtle import Turtle, Screen
+# move the snake#
+body_blocks = []
 
-#for loop to create the snake body#
-for i in range(3):
-    snake_body = Turtle('square')
-    snake_body.color('white')
-    snake_body.goto(40-i*20, 0)
-
-
-
-
+# for loop to create the snake body#
 screen = Screen()
+
 screen.bgcolor("black")
 screen.title("Welcome to the snake game")
 screen.setup(width=600, height=600)
+screen.tracer(0)
+
+for i in range(3):
+    snake_body = Turtle('square')
+    snake_body.color('white')
+    snake_body.penup()
+    snake_body.goto(40 - i * 20, 0)
+    body_blocks.append(snake_body)
+
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    for i in body_blocks:
+        i.forward(20)
+
+
+
 screen.exitonclick()
