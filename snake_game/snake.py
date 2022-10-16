ba5f84c4a4cd1718b8a1be6b85e1e28f17bddbd4
EEthanZZ <yczhu@yahoo.com>
2022-10-16 22:11:39 +1100

    fix the starting position bug and movement bug

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -7,11 +7,17 @@ START_POSITION = [(START_POSITION_RANGE, START_POSITION_RANGE),
                   (START_POSITION_RANGE-40, START_POSITION_RANGE)]
 START_DIRECTION = choice([90, 270])
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
     def __init__(self):
         self.body_blocks = []
         self.snake_initial()
+        self.head = self.body_blocks[0]
 
     def snake_initial(self):
         for i in START_POSITION:
@@ -20,23 +26,27 @@ class Snake:
             snake_body.penup()
             snake_body.goto(i)
             self.body_blocks.append(snake_body)
+            self.body_blocks[0].setheading(START_DIRECTION)
 
     def movement(self):
         for i in range(len(self.body_blocks) - 1, 0, -1):
             new_x = self.body_blocks[i - 1].xcor()
             new_y = self.body_blocks[i - 1].ycor()
             self.body_blocks[i].goto(new_x, new_y)
-        # self.body_blocks[0].setheading(START_DIRECTION)
-        self.body_blocks[0].forward(20)
+        self.head.forward(20)
 
     def snake_up(self):
-        self.body_blocks[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def snake_down(self):
-        self.body_blocks[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def snake_left(self):
-        self.body_blocks[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def snake_right(self):
-        self.body_blocks[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
