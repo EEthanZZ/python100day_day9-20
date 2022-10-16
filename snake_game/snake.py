2cd608586aaefee374bd20ef0e1a42231eea0cc6
EEthanZZ <yczhu@yahoo.com>
2022-10-16 21:34:07 +1100

    snake initial position function

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -3,3 +3,13 @@ from turtle import Turtle
 
 class Snake:
     def __init__(self):
+        self.body_blocks = []
+        self.snake_initial()
+
+    def snake_initial(self):
+        for i in range(3):
+            snake_body = Turtle('square')
+            snake_body.color('white')
+            snake_body.penup()
+            snake_body.goto(40 - i * 20, 0)
+            self.body_blocks.append(snake_body)
