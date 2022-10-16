cb43b240cec125a4c97800176d3b80425c27e13a
EEthanZZ <yczhu@yahoo.com>
2022-10-16 21:45:22 +1100

    make the random starting position

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,10 +1,11 @@
 from turtle import Turtle
-from random import randint
+from random import randint, choice
+
 START_POSITION_RANGE = randint(-240, 240)
 START_POSITION = [(START_POSITION_RANGE, START_POSITION_RANGE),
                   (START_POSITION_RANGE-20, START_POSITION_RANGE),
                   (START_POSITION_RANGE-40, START_POSITION_RANGE)]
-
+START_DIRECTION = choice([90, 270])
 
 class Snake:
     def __init__(self):
@@ -24,4 +25,5 @@ class Snake:
             new_x = self.body_blocks[i - 1].xcor()
             new_y = self.body_blocks[i - 1].ycor()
             self.body_blocks[i].goto(new_x, new_y)
+        self.body_blocks[0].left(START_DIRECTION)
         self.body_blocks[0].forward(20)
