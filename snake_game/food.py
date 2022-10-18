2de6660573c5cbe56914ed6b6979c4e2ad21e277
EEthanZZ <yczhu@yahoo.com>
2022-10-18 13:34:41 +1100

    refresh the food location every time snake head touch the food

--- snake_game/food.py
+++ snake_game/food.py
@@ -9,7 +9,9 @@ class Food(Turtle):
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
+        self.refresh()
+
+    def refresh(self):
         self.speed("fastest")
         self.goto(x=randint(-280, 280), y=randint(-280,280))
-
 
