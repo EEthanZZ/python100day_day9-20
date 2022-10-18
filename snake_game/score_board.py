446a496cb3dbe957303bbaeec8537a41de8dc919
EEthanZZ <yczhu@yahoo.com>
2022-10-18 13:48:30 +1100

    update the score board

--- snake_game/score_board.py
+++ snake_game/score_board.py
@@ -8,4 +8,13 @@ class Score_board(Turtle):
         self.hideturtle()
         self.goto(x=0, y=240)
         self.color("white")
+        self.score = 0
+        self.update_scoreboard()
 
+    def add_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", font=("Arial", 24, "normal"), align="center")
