b76e20fa8d962fae62bd9b60586a33473848d5e7
EEthanZZ <yczhu@yahoo.com>
2022-10-18 13:19:01 +1100

    food appears on the map

--- snake_game/food.py
+++ snake_game/food.py
@@ -5,6 +5,11 @@ from turtle import Turtle
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.goto(x=randint(-280, 280), y=randint(-280,280))
 
 
