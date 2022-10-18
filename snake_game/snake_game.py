4285d908b81c9172f5289cd116ed368885155abc
EEthanZZ <yczhu@yahoo.com>
2022-10-18 13:54:42 +1100

    detect the wall collision

--- snake_game/snake_game.py
+++ snake_game/snake_game.py
@@ -50,5 +50,9 @@ while is_game_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         score.add_score()
+    # 6. detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+
 
 screen.exitonclick()
