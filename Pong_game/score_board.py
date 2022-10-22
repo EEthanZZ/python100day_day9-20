af222aa01382e7426dd7a8813c5bc83f9d51598b
EEthanZZ <yczhu@yahoo.com>
2022-10-22 22:26:15 +1100

    scoreboard class

---
+++ Pong_game/score_board.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+
+    def update_score(self):
+        self.goto(0, 100)
+        self.write(f"{self.l_score} : {self.r_score}")
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_score()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_score()
