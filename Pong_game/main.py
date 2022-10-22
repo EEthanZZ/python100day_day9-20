3609620164524fcd0f308a05476550a57d6b240f
EEthanZZ <yczhu@yahoo.com>
2022-10-22 20:41:45 +1100

    task breakdown

---
+++ Pong_game/main.py
@@ -0,0 +1,18 @@
+"""
+create the screen
+
+Create and move a paddle
+
+Create the second paddle
+
+create the ball and make it move
+
+detect collision with wall and bounce
+
+detect with paddle
+
+detect when paddle misses
+
+keep score
+"""
+
