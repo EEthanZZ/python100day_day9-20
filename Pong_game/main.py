0b379555a1a3d4450ccc6bc8c22a68484ad0ef41
EEthanZZ <yczhu@yahoo.com>
2022-10-22 21:03:53 +1100

    cancel the animation

--- Pong_game/main.py
+++ Pong_game/main.py
@@ -30,13 +30,27 @@ tim.shapesize(stretch_len=1, stretch_wid=5)
 tim.shape("square")
 tim.color("white")
 tim.penup()
+screen.tracer(0)
 tim.goto(x=350, y=0)
 
 
+def go_up():
+    new_y = tim.ycor() + 20
+    tim.goto(x=350, y=new_y)
+
+
+def go_down():
+    new_y = tim.ycor() - 20
+    tim.goto(x=350, y=new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "w")
+screen.onkey(go_down, "s")
 
 
 # create the paddle
 
 
-
+screen.update()
 screen.exitonclick()
