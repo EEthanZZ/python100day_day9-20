b12c6e8d7ea64ab2dca45d783beb8c2850290583
EEthanZZ <yczhu@yahoo.com>
2022-10-22 20:59:33 +1100

    create the first paddle at the screen border

--- Pong_game/main.py
+++ Pong_game/main.py
@@ -15,16 +15,28 @@ detect when paddle misses
 
 keep score
 """
+PADDLE_LOCATION = [(350,)]
 
 # create the screen
 from turtle import Turtle, Screen
-tim = Turtle()
-
-
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Welcome to the Pong game")
 screen.setup(width=800, height=600)
+
+
+tim = Turtle()
+tim.shapesize(stretch_len=1, stretch_wid=5)
+tim.shape("square")
+tim.color("white")
+tim.penup()
+tim.goto(x=350, y=0)
+
+
+
+
+# create the paddle
+
+
+
 screen.exitonclick()
-
-
