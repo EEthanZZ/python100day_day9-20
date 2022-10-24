d1a4c67e6c579f5a16633e08cb377f8546feb35d
EEthanZZ <yczhu@yahoo.com>
2022-10-24 22:04:36 +1100

    create cars

--- turtle project/cross_road/car_manager.py
+++ turtle project/cross_road/car_manager.py
@@ -1,7 +1,26 @@
+import random
+from random import choice
+from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.all_cars = []
+
+    def create_cars(self):
+        new_car = Turtle("square")
+        new_car.shapesize(stretch_wid=2, stretch_len=1)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        y_cord = random.randint(-250, 250)
+        new_car.goto(300, y_cord)
+        self.all_cars.append(new_car)
+
+
+
+
+
