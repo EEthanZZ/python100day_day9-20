e9d146a559651b0a5cb23f5b8ef2b07d0f26c2cf
EEthanZZ <yczhu@yahoo.com>
2022-10-27 10:00:16 +1100

    new method to write the highest score

--- snake_game/score_board.py
+++ snake_game/score_board.py
@@ -9,6 +9,7 @@ class Score_board(Turtle):
         self.goto(x=0, y=240)
         self.color("white")
         self.score = 0
+        self.highscore = 0
         self.update_scoreboard()
 
     def add_score(self):
@@ -19,6 +20,11 @@ class Score_board(Turtle):
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", font=("Arial", 24, "normal"), align="center")
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"game over", font=("Arial", 24, "normal"), align="center")
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"game over", font=("Arial", 24, "normal"), align="center")
