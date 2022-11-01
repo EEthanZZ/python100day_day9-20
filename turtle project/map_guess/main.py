77f9514fa81e96e6ec51169d6dac7812def840c2
EEthanZZ <yczhu@yahoo.com>
2022-11-02 00:03:36 +1100

    get the coor by clicking on the map

--- turtle project/map_guess/main.py
+++ turtle project/map_guess/main.py
@@ -5,7 +5,16 @@ screen = turtle.Screen()
 screen.title("U.S. states game")
 image = "blank_states_img.gif"
 turtle.addshape(image)
-
 turtle.shape(image)
+
+
+def get_mouse_click_coor(x,y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
+
+
 
 screen.exitonclick()
