fd7f33ae9ae1407505a9da57b6b33b71d74b894f
EEthanZZ <yczhu@yahoo.com>
2022-11-02 15:57:34 +1100

    game loop

--- turtle project/map_guess/main.py
+++ turtle project/map_guess/main.py
@@ -6,22 +6,25 @@ screen.title("U.S. states game")
 image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
-tim = turtle.Turtle()
-# tim.hideturtle()
-# tim.penup()
 
 data = pandas.read_csv("50_states.csv")
 states_list = data["state"].to_list()
 print(states_list)
+guessed_state = []
 
-answer = screen.textinput(title="guess the state", prompt="what's the state name?").title()
-print(answer)
+while len(guessed_state) < 50:
+    answer = screen.textinput(title="guess the state", prompt="what's the state name?").title()
+    print(answer)
 
-if answer in states_list:
-    state_data = data[data["state"] == answer]
-    x_cord = int(state_data["x"])
-    y_cord = int(state_data["y"])
-    tim.goto(x_cord, y_cord)
-    tim.write(answer)
+    if answer in states_list:
+        tim = turtle.Turtle()
+        tim.hideturtle()
+        tim.penup()
+        state_data = data[data["state"] == answer]
+        x_cord = int(state_data["x"])
+        y_cord = int(state_data["y"])
+        tim.goto(x_cord, y_cord)
+        tim.write(answer)
+        guessed_state.append(answer)
 
 screen.exitonclick()
