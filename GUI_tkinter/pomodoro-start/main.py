71c46d51582b1a03fb27157903c31bc19930bafe
EEthanZZ <yczhu@yahoo.com>
2022-11-09 21:41:08 +1100

    count down format in min and sec

--- GUI_tkinter/pomodoro-start/main.py
+++ GUI_tkinter/pomodoro-start/main.py
@@ -1,4 +1,6 @@
+import math
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,15 +11,24 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_time():
+    count_down(180)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -29,14 +40,12 @@ canvas.create_image(103, 112, image=tomato)
 timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-count_down(5)
-
-start_button = Button(text="Start", highlightbackground=YELLOW)
+start_button = Button(text="Start", highlightbackground=YELLOW, command=start_time)
 start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightbackground=YELLOW)
 reset_button.grid(column=2, row=2)
 
-timer_label = Label(text="Timer", font=(FONT_NAME, 50, "bold"),fg=GREEN)
+timer_label = Label(text="Timer", font=(FONT_NAME, 50, "bold"), fg=GREEN)
 timer_label.config(bg=YELLOW)
 timer_label.grid(column=1, row=0)
 check_label = Label(text="✔︎", bg=YELLOW, fg=GREEN)
