6fff7bec9300d60066f643f258dddf24106e1a12
EEthanZZ <yczhu@yahoo.com>
2022-11-09 21:08:08 +1100

    add component

--- GUI_tkinter/pomodoro-start/main.py
+++ GUI_tkinter/pomodoro-start/main.py
@@ -24,6 +24,17 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightbackground=YELLOW)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(103, 112, image=tomato)
 canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightbackground=YELLOW)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", highlightbackground=YELLOW)
+reset_button.grid(column=2, row=2)
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 50, "bold"),fg=GREEN)
+timer_label.config(bg=YELLOW)
+timer_label.grid(column=1, row=0)
+check_label = Label(text="✔︎", bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
 
 window.mainloop()
