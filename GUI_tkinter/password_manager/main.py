701c2180430b28bd40d932934539414c15d51c95
EEthanZZ <yczhu@yahoo.com>
2022-11-10 23:58:38 +1100

    initial GUI

--- GUI_tkinter/password_manager/main.py
+++ GUI_tkinter/password_manager/main.py
@@ -1,5 +1,17 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("password manager")
+window.config(padx=10, pady=20)
+canvas = Canvas(width=200, height=200)
+lock = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock)
+canvas.pack()
+
+
+
+window.mainloop()
