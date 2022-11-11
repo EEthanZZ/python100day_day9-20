0d720204a1cdcc1030e3f84e9791773537a4da0e
EEthanZZ <yczhu@yahoo.com>
2022-11-11 15:07:11 +1100

    list comprehension

--- GUI_tkinter/password_manager/main.py
+++ GUI_tkinter/password_manager/main.py
@@ -5,32 +5,28 @@ from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+def random_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-password_list = []
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
+    password_letters = [random.choice(letters) for i in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for i in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for i in range(nr_numbers)]
 
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
+    password_list = password_numbers + password_symbols + password_letters
 
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
 
-random.shuffle(password_list)
+    random.shuffle(password_list)
 
-password = ""
-for char in password_list:
-  password += char
+    password = "".join(password_list)
+    return password_input.get(password)
 
-print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def add():
@@ -74,8 +70,8 @@ email_input.grid(column=1, row=2, columnspan=2)
 password_input = Entry()
 password_input.grid(column=1, row=3)
 
-password_icon = Button(text="generate password")
-password_icon.grid(column=2, row=3)
+password_icon = Button(text="generate password", command=random_password)
+password_icon.grid(column=2, row=3,)
 add_icon = Button(text="Add", width=36, command=add)
 add_icon.grid(column=1, row=4, columnspan=2)
 
