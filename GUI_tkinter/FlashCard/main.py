5a090f287187cf59e65cd91ab6ba1f868391c4e0
EEthanZZ <yczhu@yahoo.com>
2022-11-16 22:09:24 +1100

    change the image

--- GUI_tkinter/FlashCard/main.py
+++ GUI_tkinter/FlashCard/main.py
@@ -21,6 +21,7 @@ def next_word():
 def flip_card():
     canvas.itemconfig(title, text="English")
     canvas.itemconfig(word, text=b["English"])
+    canvas.itemconfig(card_img, image=back_photo)
 
 window = Tk()
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
@@ -29,9 +30,11 @@ window.after(3000, func=flip_card)
 
 canvas = Canvas(height=525, width=800)
 photo = PhotoImage(file="images/card_front.png")
+back_photo = PhotoImage(file="images/card_back.png")
 button_right_img = PhotoImage(file="images/right.png")
 button_wrong_img = PhotoImage(file="images/wrong.png")
-canvas.create_image(400, 263, image=photo)
+card_img = canvas.create_image(400, 263, image=photo)
+
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "bold"))
 word = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
@@ -42,10 +45,10 @@ button_wrong.grid(column=0, row=1)
 button_right = Button(image=button_right_img, highlightthickness=0, command=next_word)
 button_right.grid(column=1, row=1)
 
-back_photo = PhotoImage(file="images/card_back.png")
+
 
 next_word()
 
 
-canvas.itemconfig(photo, image=back_photo)
+
 window.mainloop()
