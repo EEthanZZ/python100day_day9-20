f63b6fd36f53a76e2c586ff10f764afe1266f6c1
EEthanZZ <yczhu@yahoo.com>
2022-11-18 23:10:39 +1100

    use of datetime module

--- API/SMTP_email/main.py
+++ API/SMTP_email/main.py
@@ -1,11 +1,20 @@
-import smtplib
+# import smtplib
+#
+# my_email = "zycc2727@gmail.com"
+# password = "wcbujxxyjdbnmjdl"
+#
+# with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email,
+#                      password=password)
+#     connection.sendmail(from_addr=my_email,to_addrs="yczhu@yahoo.com",
+#                         msg="Subject:Hello\n\nThis is body of my email.")
 
-my_email = "zycc2727@gmail.com"
-password = "wcbujxxyjdbnmjdl"
+import datetime as dt
 
-with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
-    connection.starttls()
-    connection.login(user=my_email,
-                     password=password)
-    connection.sendmail(from_addr=my_email,to_addrs="yczhu@yahoo.com",
-                        msg="Subject:Hello\n\nThis is body of my email.")
+now = dt.datetime.now()
+print(now)
+year = now.year
+month = now.month
+weekday = now.weekday()
+print(year)
