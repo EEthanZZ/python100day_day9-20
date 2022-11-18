c468a6200843ed97761731b9f5d108ba368b1993
EEthanZZ <yczhu@yahoo.com>
2022-11-18 23:03:57 +1100

    add a subject, with

--- API/SMTP_email/main.py
+++ API/SMTP_email/main.py
@@ -3,10 +3,9 @@ import smtplib
 my_email = "zycc2727@gmail.com"
 password = "wcbujxxyjdbnmjdl"
 
-connection = smtplib.SMTP("smtp.gmail.com",port=587)
-connection.starttls()
-connection.login(user=my_email,
-                 password=password)
-connection.sendmail(from_addr=my_email,to_addrs="yczhu@yahoo.com",
-                    msg="Hello")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email,
+                     password=password)
+    connection.sendmail(from_addr=my_email,to_addrs="yczhu@yahoo.com",
+                        msg="Subject:Hello\n\nThis is body of my email.")
