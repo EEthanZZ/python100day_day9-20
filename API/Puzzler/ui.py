cd90181752691c9623d68caaa182a25a3a5c9802
EEthanZZ <yczhu@yahoo.com>
2022-11-30 08:49:30 +1100

    gui & data interation

--- API/Puzzler/ui.py
+++ API/Puzzler/ui.py
@@ -1,9 +1,34 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
-
+FONT = ("Arial", 20, "italic")
 class QuizUI:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
+        self.window.config(bg=THEME_COLOR, pady=20, padx=20)
+        self.canvas = Canvas(height=250, width=300)
+        self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=50)
+        self.q_next = self.canvas.create_text(150, 125, width=250, text="word", font=FONT, fill=THEME_COLOR)
+
+        self.right_photo = PhotoImage(file="images/true.png")
+        self.wrong_photo = PhotoImage(file="images/false.png")
+
+        self.button_right = Button(image=self.right_photo, highlightthickness=0)
+        self.button_right.grid(column=0, row=2, padx=20, pady=20)
+        self.button_wrong = Button(image=self.wrong_photo, highlightthickness=0)
+        self.button_wrong.grid(column=1, row=2, padx=20, pady=20)
+
+        self.score_text = Label(text=f"Score:", bg=THEME_COLOR, font=FONT, fg="white")
+        self.score_text.grid(row=0, column=1, padx=20, pady=20)
+
         self.window.title("Quiz")
 
+        self.get_next_q()
+
         self.window.mainloop()
+
+    def get_next_q(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.q_next, text=q_text)
