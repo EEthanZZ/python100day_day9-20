d6d03d3ffe73ffcc49534754b36f075a1e21af2b
EEthanZZ <yczhu@yahoo.com>
2022-12-01 10:07:45 +1100

    umbrella prompt

--- API/RainMinder/main.py
+++ API/RainMinder/main.py
@@ -7,10 +7,10 @@ PART = "current,daily,minutely"
 OWN_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
 
 
-connection = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LAT}&lon={MY_LNG}"
-                              f"&exclude={PART}&appid={MY_API_KEY}")
-weather = connection.json()
-print(weather)
+# connection = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LAT}&lon={MY_LNG}"
+#                               f"&exclude={PART}&appid={MY_API_KEY}")
+# weather = connection.json()
+# print(weather)
 
 weather_param = {
     "lat": MY_LAT,
@@ -23,15 +23,21 @@ response = requests.get(OWN_ENDPOINT, params=weather_param)
 weather_data = response.json()
 print(weather_data)
 
-for i in range(0, 12):
-    hourly12_weather = weather_data["hourly"][i]["weather"][0]["id"]
-    print(hourly12_weather)
-print()
-print()
+# for i in range(0, 12):
+#     hourly12_weather = weather_data["hourly"][i]["weather"][0]["id"]
+#     print(hourly12_weather)
+# print()
+# print()
 
+will_rain = False
 weather = weather_data["hourly"][0:12]
 for hourly_weather in weather:
     code = hourly_weather["weather"][0]["id"]
-    print(code)
+    if code < 700:
+        will_rain = True
 
+if will_rain:
+    print("bring an umbrella")
+else:
+    print("no umbrella")
 
