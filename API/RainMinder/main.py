11d7a4bf455c74a0ef8d6a757338766bfdee7ebc
EEthanZZ <yczhu@yahoo.com>
2022-12-01 10:21:24 +1100

    twilio set up

--- API/RainMinder/main.py
+++ API/RainMinder/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 MY_API_KEY = "5b3d56f17ad033dc4198740bc0c89ba9"
 MY_LAT = -37.832642
@@ -6,6 +7,9 @@ MY_LNG = 145.125031
 PART = "current,daily,minutely"
 OWN_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
 
+TWILIO_SID = "AC185eb2acbcdee217a8e18b6a364a56bf"
+TWILIO_AUTH_TOKEN = "84b5b2e3595ce29205c2a3b7b6660377"
+TWILIO_NUM = "+16614864166"
 
 # connection = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LAT}&lon={MY_LNG}"
 #                               f"&exclude={PART}&appid={MY_API_KEY}")
@@ -37,7 +41,12 @@ for hourly_weather in weather:
         will_rain = True
 
 if will_rain:
-    print("bring an umbrella")
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages \
+        .create(
+        body="bring an umbrella with you",
+        from_=TWILIO_NUM,
+        to="+61404346835"
+    )
 else:
     print("no umbrella")
-
