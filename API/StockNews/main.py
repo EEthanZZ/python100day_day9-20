3445b6129bf9613de5f4cc2d15b9e18d3e8d4134
EEthanZZ <yczhu@yahoo.com>
2022-12-02 09:46:09 +1100

    NEWS API

--- API/StockNews/main.py
+++ API/StockNews/main.py
@@ -1,5 +1,10 @@
 import requests
 from datetime import date, timedelta
+
+yesterday = str(date.today() - timedelta(days=1))
+day_before_yesterday = str(date.today() - timedelta(days=2))
+print(yesterday)
+print(day_before_yesterday)
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -10,11 +15,17 @@ stock_para = {
     "symbol": STOCK_NAME,
     "apikey": STOCK_API
 }
-yesterday = str(date.today() - timedelta(days=1))
-day_before_yesterday = str(date.today() - timedelta(days=2))
-print(yesterday)
-print(day_before_yesterday)
+
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_API = "1732c7396c084730ae406cdff96427d1"
+news_para = {
+    "q": "tesla",
+    "from": f"{yesterday}",
+    "apiKey": NEWS_API,
+}
+
+
+
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -26,21 +37,22 @@ stock_prices = data["Time Series (Daily)"]
 stock_yesterday = stock_prices[yesterday]
 stock_yesterday_close = float(stock_yesterday['4. close'])
 print(stock_yesterday_close)
-#TODO 2. - Get the day before yesterday's closing stock price
+
 stock_before_yesterday_close = float(stock_prices[day_before_yesterday]['4. close'])
 print(stock_before_yesterday_close)
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+
 difference = abs(stock_yesterday_close - stock_before_yesterday_close)
 print(difference)
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+
 difference_in_percentage = difference / stock_before_yesterday_close
 print(difference_in_percentage)
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+if difference_in_percentage == 0:
+    print("get news")
+    ## STEP 2: https://newsapi.org/
+    response_new = requests.get(url=NEWS_ENDPOINT, params=news_para)
+    news = response_new.json()
+    print(news["articles"][0:2])
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
