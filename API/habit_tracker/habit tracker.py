9d7d1eb006879ec8f1d6825dacecc97c2f78456d
EEthanZZ <yczhu@yahoo.com>
2022-12-03 20:18:01 +1100

    POST the graph

--- API/habit_tracker/habit tracker.py
+++ API/habit_tracker/habit tracker.py
@@ -1,11 +1,26 @@
 import requests
-
+USERNAME = "ethanz"
+TOKEN = "aaaabbbbcccc"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 PIXELA_para = {
-    "token": "aaaabbbbcccc",
-    "username": "ethanz",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-response = requests.post(url=PIXELA_ENDPOINT, json=PIXELA_para)
+# response = requests.post(url=PIXELA_ENDPOINT, json=PIXELA_para)
+# print(response.text)
+PIXELA_GRAPH = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+graph_para = {
+    "id": "graph1",
+    "name": "Daily Water",
+    "unit": "cups",
+    "type": "int",
+    "color": "shibafu"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=PIXELA_GRAPH, json=graph_para, headers=headers)
 print(response.text)
