b3b9fb0efcff2dd24d6226e03f8c7858f6671dfd
EEthanZZ <yczhu@yahoo.com>
2022-12-06 00:07:26 +1100

    testing

--- API/Flight_ticket/flight_search.py
+++ API/Flight_ticket/flight_search.py
@@ -1,15 +1,9 @@
 import requests
 from datetime import datetime
 
+SEARCH_ENDPOINT = "https://tequila-api.kiwi.com"
+SEARCH_API_KEY = "MxIBZFGvcU2vQ7zj8V3Zz-nv7Zwonn69"
 class FlightSearch:
-    def __init__(self):
-        self.SEARCH_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
-        self.SEARCH_API_KEY = "MxIBZFGvcU2vQ7zj8V3Zz-nv7Zwonn69"
-        self.today = datetime.now().strftime("%d/%/%Y")
-        self.SEARCH_PARA = {
-            "fly_from": "",
-            "date_from": self.today,
-            "date_to": "07/12/2022"
-        }
-
-        self.response = requests.get(url=self.SEARCH_ENDPOINT, params=self.SEARCH_PARA, headers=self.SEARCH_API_KEY)
+    def get_dest_code(self, city_name):
+        code = "Testing"
+        return code
