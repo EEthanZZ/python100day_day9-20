37eb33728a75e1cfb310e002f1a6f8bb5aba2763
EEthanZZ <yczhu@yahoo.com>
2022-12-06 23:52:37 +1100

    fill the ITAT CODE column with airport name in SHEETY

--- API/Flight_ticket/flight_search.py
+++ API/Flight_ticket/flight_search.py
@@ -5,5 +5,19 @@ SEARCH_ENDPOINT = "https://tequila-api.kiwi.com"
 SEARCH_API_KEY = "MxIBZFGvcU2vQ7zj8V3Zz-nv7Zwonn69"
 class FlightSearch:
     def get_dest_code(self, city_name):
-        code = "Testing"
+        location_endpoint = f"{SEARCH_ENDPOINT}/locations/query"
+        headers = {
+            "apikey": SEARCH_API_KEY
+        }
+        query = {
+            "term": city_name,
+            "location_types": "city"
+        }
+        response = requests.get(
+            url=location_endpoint,
+            headers=headers,
+            params=query
+        )
+        result = response.json()["locations"]
+        code = result[0]["code"]
         return code
