7d99043723c76963f76ff67802b50e776b3cd521
EEthanZZ <yczhu@yahoo.com>
2022-12-07 11:41:57 +1100

    built flight data

---
+++ API/Flight_ticket/flight_data.py
@@ -0,0 +1,9 @@
+class FlightData:
+    def __init__(self, price, dept_city, dept_air, dest_city, dest_air, dept_date, return_date):
+        self.price = price
+        self.dept_city = dept_city
+        self.dept_air = dept_air
+        self.dest_air = dest_air
+        self.dest_city = dest_city
+        self.dept_date = dept_date
+        self.return_date = return_date
--- API/Flight_ticket/flight_search.py
+++ API/Flight_ticket/flight_search.py
@@ -1,8 +1,14 @@
 import requests
-from datetime import datetime
+from flight_data import FlightData
+from datetime import datetime, timedelta
+from data_manager import DataManager
 
+today = datetime.now().strftime("%d/%m/%Y")
+today_after_60days = (datetime.now() + timedelta(days=180)).strftime("%d/%m/%Y")
 SEARCH_ENDPOINT = "https://tequila-api.kiwi.com"
 SEARCH_API_KEY = "MxIBZFGvcU2vQ7zj8V3Zz-nv7Zwonn69"
+
+
 class FlightSearch:
     def get_dest_code(self, city_name):
         location_endpoint = f"{SEARCH_ENDPOINT}/locations/query"
@@ -21,3 +27,40 @@ class FlightSearch:
         result = response.json()["locations"]
         code = result[0]["code"]
         return code
+
+    def flight_ticket(self, dept_ct_code, dest_ct_code, from_time, to_time):
+        ticket_search_endpoint = f"{SEARCH_ENDPOINT}/search"
+        headers = {
+            "apikey": SEARCH_API_KEY
+        }
+        query = {
+            "fly_from": dept_ct_code,
+            "fly_to": dest_ct_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "curr": "AUD",
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+        }
+        response = requests.get(url=ticket_search_endpoint,
+                                params=query,
+                                headers=headers)
+        # try:
+        #     data = response.json()["data"][0]
+        # except IndexError
+        data = response.json()["data"][0]
+        print(data)
+        flight_data = FlightData(
+            price=data["price"],
+            dept_city=data["route"][0]["cityFrom"],
+            dept_air=data["route"][0]["flyFrom"],
+            dest_air=data["route"][0]["flyTo"],
+            dest_city=data["route"][0]["cityTo"],
+            dept_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0],
+        )
+        print(f"{flight_data.dest_city}: £{flight_data.price}")
+        return flight_data
