14e0038a294365c06fdf77c2631cee867cb0474c
EEthanZZ <yczhu@yahoo.com>
2022-12-07 12:50:04 +1100

    search for the lowest ticket

--- API/Flight_ticket/flight_search.py
+++ API/Flight_ticket/flight_search.py
@@ -1,11 +1,7 @@
 import requests
 from flight_data import FlightData
-from datetime import datetime, timedelta
-from data_manager import DataManager
 
-today = datetime.now().strftime("%d/%m/%Y")
-today_after_60days = (datetime.now() + timedelta(days=180)).strftime("%d/%m/%Y")
-SEARCH_ENDPOINT = "https://tequila-api.kiwi.com"
+SEARCH_ENDPOINT = "https://api.tequila.kiwi.com"
 SEARCH_API_KEY = "MxIBZFGvcU2vQ7zj8V3Zz-nv7Zwonn69"
 
 
@@ -26,10 +22,10 @@ class FlightSearch:
         )
         result = response.json()["locations"]
         code = result[0]["code"]
+        print(code)
         return code
 
     def flight_ticket(self, dept_ct_code, dest_ct_code, from_time, to_time):
-        ticket_search_endpoint = f"{SEARCH_ENDPOINT}/search"
         headers = {
             "apikey": SEARCH_API_KEY
         }
@@ -38,21 +34,22 @@ class FlightSearch:
             "fly_to": dest_ct_code,
             "date_from": from_time.strftime("%d/%m/%Y"),
             "date_to": to_time.strftime("%d/%m/%Y"),
-            "curr": "AUD",
+            "curr": "GBP",
             "nights_in_dst_from": 7,
             "nights_in_dst_to": 28,
             "flight_type": "round",
             "one_for_city": 1,
             "max_stopovers": 0,
         }
-        response = requests.get(url=ticket_search_endpoint,
+        response = requests.get(url=f"{SEARCH_ENDPOINT}/v2/search",
                                 params=query,
                                 headers=headers)
-        # try:
-        #     data = response.json()["data"][0]
-        # except IndexError
-        data = response.json()["data"][0]
-        print(data)
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {dest_ct_code}.")
+            return None
+
         flight_data = FlightData(
             price=data["price"],
             dept_city=data["route"][0]["cityFrom"],
@@ -60,7 +57,7 @@ class FlightSearch:
             dest_air=data["route"][0]["flyTo"],
             dest_city=data["route"][0]["cityTo"],
             dept_date=data["route"][0]["local_departure"].split("T")[0],
-            return_date=data["route"][1]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
         )
         print(f"{flight_data.dest_city}: £{flight_data.price}")
         return flight_data
--- API/Flight_ticket/main.py
+++ API/Flight_ticket/main.py
@@ -1,14 +1,35 @@
+from datetime import datetime, timedelta
+
 from data_manager import DataManager
 data_manager = DataManager()
 sheet_data = data_manager.get_dest()
+from flight_search import FlightSearch
+search = FlightSearch()
+from notification_manager import NotificationManager
+notification_manager = NotificationManager()
 
 
+tomorrow = (datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y")
+today_after_60days = (datetime.now() + timedelta(days=180)).strftime("%d/%m/%Y")
+ORIGINAL_CITY = "LON"
+print(tomorrow)
+print(sheet_data)
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
     for i in sheet_data:
-        i["iataCode"] = flight_search.get_dest_code(i["city"])
-    print(f"{sheet_data}")
-    data_manager.get_dest = sheet_data
+        i["itatCode"] = search.get_dest_code(i["city"])
+    sheet_data = data_manager.get_dest()
     data_manager.get_dest_code()
+
+    for des in sheet_data:
+        flight = search.flight_ticket(
+            dept_ct_code=ORIGINAL_CITY,
+            dest_ct_code=des["iataCode"],
+            from_time=tomorrow,
+            to_time=today_after_60days
+        )
+    if flight.price < des["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
+
 
