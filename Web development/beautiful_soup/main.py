907b6f937a36c60efc5f4d1a9202a933ea50d3f2
EEthanZZ <yczhu@yahoo.com>
2022-12-12 21:44:51 +1100

    select

--- Web development/beautiful_soup/main.py
+++ Web development/beautiful_soup/main.py
@@ -12,3 +12,12 @@ print(a.string)
 all_anchor_tag = soup.find_all(name="a")
 for tag in all_anchor_tag:
     print(f"{tag.getText()}: {tag.get('href')}")
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+sec_heading = soup.find(name="h3", class_="heading")
+print(sec_heading)
+
+company_url = soup.select(selector=".heading")
+print(company_url)
