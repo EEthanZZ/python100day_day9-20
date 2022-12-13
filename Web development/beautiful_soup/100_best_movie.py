44accb8721e8cc414a658426e43d01d370e77920
EEthanZZ <yczhu@yahoo.com>
2022-12-13 21:50:07 +1100

    extract the movies from timeout

--- Web development/beautiful_soup/100_best_movie.py
+++ Web development/beautiful_soup/100_best_movie.py
@@ -7,9 +7,12 @@ soup = BeautifulSoup(movies, "html.parser")
 
 movies_100 = soup.find_all(name="h3", class_="_h3_cuogz_1")
 movies_name = [i.getText() for i in movies_100]
+movies = []
 for i in movies_name:
     a = str("\xa0")
     if a in i:
         x = i.replace(a, "")
-    print(x)
+        movies.append(x)
+
+
 
