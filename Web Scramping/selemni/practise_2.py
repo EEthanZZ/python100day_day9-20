b1beff991e5c2dc7a8f7f3239313e45f42625ff1
EEthanZZ <yczhu@yahoo.com>
2022-12-18 20:11:34 +1100

    auto register

--- Web Scramping/selemni/practise_2.py
+++ Web Scramping/selemni/practise_2.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-url = "https://en.wikipedia.org/wiki/Main_Page"
+url = "https://web.archive.org/web/20220120120408/https://secure-retreat-92358.herokuapp.com/"
 op = webdriver.ChromeOptions()
 op.add_experimental_option("detach", True)
 chrome = "C:/chromedriver_win32/chromedriver"
@@ -10,8 +11,9 @@ service = Service(chrome)
 browser = webdriver.Chrome(service=service, options=op)
 browser.get(url)
 
-number = browser.find_element(By.CSS_SELECTOR, "#articlecount a")
-print(number.text)
-
-search = browser.find_element(By.NAME, "search")
-search.send_keys("python")
+first_name = browser.find_element(By.NAME, "fName")
+first_name.send_keys("Ethan")
+last_name = browser.find_element(By.NAME, "lName")
+last_name.send_keys("Z")
+email = browser.find_element(By.NAME, "email")
+email.send_keys("aa@yahoo.com")
