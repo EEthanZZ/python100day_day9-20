4e4b6bfdcae3c5054130330f42723d7efa716953
EEthanZZ <yczhu@yahoo.com>
2022-12-18 18:00:15 +1100

    extract price

--- Web Scramping/selemni/main.py
+++ Web Scramping/selemni/main.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
+url = "https://www.amazon.com/dp/B0053WRWX8/ref=sbl_dpx_kitchen-electric-cookware_B09F6XHB4C_0?th=1"
 op = webdriver.ChromeOptions()
+op.add_experimental_option("detach", True)
 chrome = "C:/chromedriver_win32/chromedriver"
 service = Service(chrome)
 browser = webdriver.Chrome(service=service, options=op)
-browser.get("https://www.google.com")
+browser.get(url)
+price = browser.find_element(By.CLASS_NAME, "a-price-whole")
+print(price.text)
 browser.close()
 
