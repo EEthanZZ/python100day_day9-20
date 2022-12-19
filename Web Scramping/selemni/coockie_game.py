967403cb0236a5262131941919dda95288a40fd7
EEthanZZ <yczhu@yahoo.com>
2022-12-19 14:58:00 +1100

    extract store items

--- Web Scramping/selemni/coockie_game.py
+++ Web Scramping/selemni/coockie_game.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
+
 
 url = "http://orteil.dashnet.org/experiments/cookie/"
 op = webdriver.ChromeOptions()
@@ -13,7 +15,22 @@ browser.get(url)
 
 button = browser.find_element(By.ID, "cookie")
 
-store = browser.find_element(By.ID, "store")
+timeout = time.time() + 2
+five_min = time.time() + 300
 while True:
     button.click()
+
+    if time.time() > timeout:
+        money = int(browser.find_element(By.ID, "money").text)
+
+    store = browser.find_elements(By.CSS_SELECTOR, "#store b")
+    for upgrade in store[::-1]:
+        upgrade_text = upgrade.text
+        if upgrade_text != "":
+            upgrades = upgrade_text.split("-")
+            product = upgrades[0].strip()
+            price = int(upgrades[1].strip().replace(",", ""))
+
+
+
 
