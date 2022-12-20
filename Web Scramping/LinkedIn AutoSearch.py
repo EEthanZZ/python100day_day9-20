838117386ce356e03843459aced0a1bb11a5ec1b
EEthanZZ <yczhu@yahoo.com>
2022-12-20 21:27:07 +1100

    click sign in

--- Web Scramping/LinkedIn AutoSearch.py
+++ Web Scramping/LinkedIn AutoSearch.py
@@ -1,8 +1,12 @@
+import os
+import time
 import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
 url = "https://www.linkedin.com/jobs/search/?currentJobId=3162045938&distance=25&f_AL=true&geoId=100992797&keywords=information%20technology%20support%20technician&location=Melbourne%2C%20Victoria%2C%20Australia&refresh=true&sortBy=R"
 op = webdriver.ChromeOptions()
 op.add_experimental_option("detach", True)
@@ -10,3 +14,7 @@ chrome = "C:/chromedriver_win32/chromedriver"
 service = Service(chrome)
 browser = webdriver.Chrome(service=service, options=op)
 browser.get(url)
+
+time.sleep(2)
+sign_in = browser.find_element(By.LINK_TEXT, "Sign in")
+sign_in.click()
