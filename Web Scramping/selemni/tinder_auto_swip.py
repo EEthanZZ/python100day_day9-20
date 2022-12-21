56d0b9801d85d25c6dedff51375de74dbefcd279
EEthanZZ <yczhu@yahoo.com>
2022-12-21 22:58:15 +1100

    Login to FB account

--- Web Scramping/selemni/tinder_auto_swip.py
+++ Web Scramping/selemni/tinder_auto_swip.py
@@ -16,7 +16,20 @@ chrome = "C:/chromedriver_win32/chromedriver"
 service = Service(chrome)
 browser = webdriver.Chrome(service=service, options=op)
 browser.get(url)
+browser.current_window_handle
+browser.maximize_window()
 
+time.sleep(1)
 browser.find_element(By.LINK_TEXT, "Log in").click()
 time.sleep(1)
+
 browser.find_element(By.XPATH, '//*[@id="q-839802255"]/main/div/div[1]/div/div/div[3]/span/div[2]/button').click()
+time.sleep(1)
+
+login_page = browser.window_handles[1]
+browser.switch_to.window(login_page)
+
+browser.find_element(By.XPATH, '//*[@id="email"]').send_keys(USERNAME)
+browser.find_element(By.XPATH, '//*[@id="pass"]').send_keys(PASSWORD)
+browser.find_element(By.XPATH, '//*[@id="pass"]').send_keys(Keys.ENTER)
+
