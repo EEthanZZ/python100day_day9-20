a147b6443d64783b3d418d323876a124085d75ff
EEthanZZ <yczhu@yahoo.com>
2022-12-24 12:14:07 +1100

    on the cliet page

--- Web Scramping/selemni/tinder_auto_swip.py
+++ Web Scramping/selemni/tinder_auto_swip.py
@@ -16,9 +16,8 @@ chrome = "C:/chromedriver_win32/chromedriver"
 service = Service(chrome)
 browser = webdriver.Chrome(service=service, options=op)
 browser.get(url)
-browser.current_window_handle
-browser.maximize_window()
 
+browser.maximize_window()
 time.sleep(1)
 browser.find_element(By.LINK_TEXT, "Log in").click()
 time.sleep(1)
@@ -26,10 +25,21 @@ time.sleep(1)
 browser.find_element(By.XPATH, '//*[@id="q-839802255"]/main/div/div[1]/div/div/div[3]/span/div[2]/button').click()
 time.sleep(1)
 
+main_window = browser.window_handles[0]
 login_page = browser.window_handles[1]
 browser.switch_to.window(login_page)
 
 browser.find_element(By.XPATH, '//*[@id="email"]').send_keys(USERNAME)
 browser.find_element(By.XPATH, '//*[@id="pass"]').send_keys(PASSWORD)
 browser.find_element(By.XPATH, '//*[@id="pass"]').send_keys(Keys.ENTER)
+time.sleep(5)
+# browser.maximize_window()
+# time.sleep(2)
+# browser.find_element(By.XPATH, '//*[@id="mount_0_0_5F"]/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div/div/div').click()
+# time.sleep(5)
+# browser.find_element(By.XPATH, '//*[@id="q-839802255"]/main/div/div[1]/div/div[2]/div/input').send_keys("404346835")
 
+browser.switch_to.window(main_window)
+browser.find_element(By.XPATH, '//*[@id="q-839802255"]/main/div/div/div/div[3]/button[1]').click()
+time.sleep(1)
+browser.find_element(By.XPATH, '//*[@id="q-839802255"]/main/div/div/div/div[3]/button[1]').click()
