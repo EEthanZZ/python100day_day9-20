5972058a7a63e5dbb5a879236016c339e62637e0
EEthanZZ <yczhu@yahoo.com>
2023-01-03 21:25:41 +1100

    variable rules & debug mode

--- Flask/myproject/hello.py
+++ Flask/myproject/hello.py
@@ -2,6 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/sayhi")
+def say_hi():
+    return "<p>Hi!!</p>"
+
+
+@app.route("/<name>")
+def greeting(name):
+    return f"Hi, {name}"
+if __name__ == "__main__":
+    app.run(debug=True)
