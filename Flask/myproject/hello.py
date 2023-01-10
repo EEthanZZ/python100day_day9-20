9ed9d6e3eefceb4643bda097f67c0bd75b906545
EEthanZZ <yczhu@yahoo.com>
2023-01-10 21:19:09 +1100

    add decorate function

--- Flask/myproject/hello.py
+++ Flask/myproject/hello.py
@@ -3,17 +3,38 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapped():
+        return "<b>" + function() + "</b>"
+    return wrapped
+
+
+def make_em(function):
+    def wrapped():
+        return "<em>" + function() + "</em>"
+    return wrapped
+
+
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1 style='text-align: center'>Hello, World!</h1>" \
+           "<p>this is a second line</p>" \
+           '<img src="https://media.giphy.com/media/V3Z76ctCO3jG0/giphy.gif" width=200>'
+
 
 @app.route("/sayhi")
+@make_bold
+@make_em
 def say_hi():
     return "<p>Hi!!</p>"
 
 
-@app.route("/<name>")
-def greeting(name):
-    return f"Hi, {name}"
+@app.route("/<name>/<number>")
+def greeting(name, number):
+    return f"Hi, {name}, you are {number} years old"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
