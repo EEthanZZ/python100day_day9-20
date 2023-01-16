956e2b0d593cafc072b9fc6a17653e49182eb620
EEthanZZ <yczhu@yahoo.com>
2023-01-16 23:31:46 +1100

    dynamically update the year of current

--- Flask Jinja/server_jinja.py
+++ Flask Jinja/server_jinja.py
@@ -1,12 +1,13 @@
 import random
 from flask import Flask, render_template
-
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def start():
     random_number = random.randint(1, 10)
-    return render_template("index.html", num=random_number)
+    this_year = datetime.date.today().year
+    return render_template("index.html", num=random_number, year=this_year)
 
 
 if __name__ == "__main__":
